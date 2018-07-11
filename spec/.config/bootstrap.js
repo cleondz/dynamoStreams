203e7929ea7a7cffc56cde8a09d73f5207a7ebd9
noisygerman <mwesterholz@gmail.com>
2018-07-11 11:08:40 -0700

    Initial commit

---
+++ spec/.config/bootstrap.js
@@ -0,0 +1,13 @@
+const sinonChai = require( 'sinon-chai' );
+const chai      = require( 'chai' );
+
+chai.use( sinonChai );
+
+global.expect
+  = chai.expect;
+
+global.sinon
+  = require( 'sinon' );
+
+global.resolveUitPath
+  = require( './resolveUitPathWithName' );
